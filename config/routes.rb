a942395d7636a5950d7be2c9fb40020180c59985
Janis Vaneylen <janis.vaneylen@gmail.com>
2020-02-17 14:30:29 +0100

    exercise solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/ask", to: "questions#ask"
+  # get 'about', to: 'pages#about', as: :about
   get "/answer", to: "questions#answer"
 end
