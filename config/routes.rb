e35be432971887d6eb0316932cac5e6aefbba14e
Janis Vaneylen <janis.vaneylen@gmail.com>
2020-05-05 11:41:34 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/ask", to: "questions#ask"
-  # get 'about', to: 'pages#about', as: :about
-  get "/answer", to: "questions#answer"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
